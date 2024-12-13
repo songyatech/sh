754a55fac94ee2d638db9ef88798fefd15fc34be
songyatech <tech@panw3i.com>
2024-12-13 17:42:28 +0800

    Update youtube.js

--- youtube.js
+++ youtube.js
@@ -1,17 +1,13 @@
 [rewrite_local]
-# 拒绝 googlevideo.com 和 youtubei.googleapis.com 的 UDP 连接
-^https?:\/\/([\w-]+\.)?googlevideo\.com\/.* udp reject
-^https?:\/\/youtubei\.googleapis\.com\/.* udp reject
+# 拒绝 UDP 连接
+^https?:\/\/([\w-]+\.)?googlevideo\.com\/.* url reject-dict
+^https?:\/\/youtubei\.googleapis\.com\/.* url reject-dict
 
-# 修改 YouTube API 响应
+# YouTube API 响应处理
 ^https:\/\/youtubei\.googleapis\.com\/youtubei\/v1\/(browse|next|player|search|reel\/reel_watch_sequence|guide|account\/get_setting|get_watch) url script-response-body https://raw.githubusercontent.com/Maasea/sgmodule/master/Script/Youtube/dist/youtube.response.preview.js
 
-# 映射本地请求
-^https?:\/\/[\w-]+\.googlevideo\.com\/initplayback.+&oad url 502
-
-[script]
-#  定义脚本的额外参数
-youtube_args = type=http-response,script-path=https://raw.githubusercontent.com/Maasea/sgmodule/master/Script/Youtube/dist/youtube.response.preview.js,argument="{\"lyricLang\":\"{{{歌词翻译语言}}}\",\"captionLang\":\"{{{字幕翻译语言}}}\",\"blockUpload\":{{{屏蔽上传按钮}}},\"blockImmersive\":{{{屏蔽选段按钮}}},\"debug\":{{{启用调试模式}}}}"
+# 处理广告请求
+^https?:\/\/[\w-]+\.googlevideo\.com\/initplayback.+&oad url reject-200
 
 [mitm]
 hostname = *.googlevideo.com, youtubei.googleapis.com
